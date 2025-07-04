84d4ab40d4c989f0f5295898dfdeb10166d56a31
shantanu.borkar <shantanu.borkar@orbiz.one>
2025-07-04 17:52:04 +0530

    update: txn models

--- models/transactions/create_transaction_reqest.go
+++ models/transactions/create_transaction_reqest.go
@@ -1,14 +1,11 @@
 package transactions
 
-import (
-	"github.com/google/uuid"
-)
-
 type CreateTransactionRequest struct {
-	Amount      float64    `json:"amount" validate:"required,gt=0"`
-	Type        string     `json:"type" validate:"required,oneof=expense income"`
-	Name        string     `json:"name" validate:"required,min=1"`
-	Date        string     `json:"date" validate:"required,datetime"`
-	Note        string     `json:"note" validate:"omitempty"`
-	CategoryIDs *uuid.UUID `json:"category_ids" validate:"omitempty,uuid4"` // optional
+	Amount      float64 `json:"amount" validate:"required,gt=0"`
+	Type        string  `json:"type" validate:"required,oneof=expense income"`
+	Name        string  `json:"name" validate:"required,min=1"`
+	Date        string  `json:"date" validate:"required,datetime"`
+	Note        string  `json:"note" validate:"omitempty"`
+	CategoryIDs string  `json:"category_ids" validate:"omitempty,uuid4"` // optional
+	BudgetID    string  `json:"budget_id" validate:"omitempty,uuid4"`
 }
--- models/transactions/update_transaction_request.go
+++ models/transactions/update_transaction_request.go
@@ -1,12 +1,11 @@
 package transactions
 
-import "github.com/google/uuid"
-
 type UpdateTransactionRequest struct {
-	Amount     *float64   `json:"amount" validate:"omitempty,gt=0"`
-	Type       *string    `json:"type" validate:"omitempty,oneof=expense income"`
-	Date       *string    `json:"date" validate:"omitempty,datetime"`
-	Note       *string    `json:"note"`
-	CategoryID *uuid.UUID `json:"category_id" validate:"omitempty,uuid4"`
-	Name       *string    `json:"name" validate:"omitempty,min=1"`
+	Amount     *float64 `json:"amount" validate:"omitempty,gt=0"`
+	Type       *string  `json:"type" validate:"omitempty,oneof=expense income"`
+	Date       *string  `json:"date" validate:"omitempty,datetime"`
+	Note       *string  `json:"note"`
+	CategoryID *string  `json:"category_id" validate:"omitempty,uuid4"`
+	BudgetID   *string  `json:"budget_id" validate:"omitempty,uuid4"`
+	Name       *string  `json:"name" validate:"omitempty,min=1"`
 }
